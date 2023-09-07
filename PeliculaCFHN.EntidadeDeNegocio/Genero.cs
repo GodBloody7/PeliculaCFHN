12534e6734524b1067a0c1bf623c72a78f520263
Henry <henryjeremyas@gmail.com>
2023-09-07 11:42:52 -0600

    Estructura del proyecto

---
+++ PeliculaCFHN.EntidadeDeNegocio/Genero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeliculaCFHN.EntidadeDeNegocio
+{
+    public class Genero
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(00, ErrorMessage = "Maximo 00 caracteres")]
+        public string Nombre { get; set; }
+
+        [NotMapped]
+        public string Top_Aux { get; set; }
+
+    }
+}
