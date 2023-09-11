7963cf34ecec6a3f520a96776a65d530f4f56f75
Rafa-X0 <rafaelhernandez.gz@gmail.com>
2023-09-11 10:00:18 -0600

    Codificacion de las capas BL de Genero y Rol

---
+++ PeliculaCFHN.LogicaDeNegocio/GeneroBL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PeliculaCFHN.AccesoADatos;
+using PeliculaCFHN.EntidadeDeNegocio;
+
+namespace PeliculaCFHN.LogicaDeNegocio
+{
+    public class GeneroBL
+    {
+        public async Task<int> CrearAsync(Genero pGenero)
+        {
+            return await GeneroDAL.CrearAsync(pGenero);
+        }
+        public async Task<int> ModificarAsync(Genero pGenero)
+        {
+            return await GeneroDAL.ModificarAsync(pGenero);
+        }
+        public async Task<int> EliminarAsync(Genero pGenero)
+        {
+            return await GeneroDAL.EliminarAsync(pGenero);
+        }
+        public async Task<Genero> ObtenerPorIdAsync(Genero pGenero)
+        {
+            return await GeneroDAL.OptenerPorIdAsync(pGenero);
+        }
+        public async Task<List<Genero>> ObtenerTodosAsync()
+        {
+            return await GeneroDAL.OptenerTodosAsync();
+        }
+        public async Task<List<Genero>> BuscarAsync(Genero pGenero)
+        {
+            return await GeneroDAL.BuscarAsync(pGenero);
+        }
+    }
+}
---
+++ PeliculaCFHN.LogicaDeNegocio/RolBL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PeliculaCFHN.AccesoADatos;
+using PeliculaCFHN.EntidadeDeNegocio;
+
+namespace PeliculaCFHN.LogicaDeNegocio
+{
+    public class RolBL
+    {
+        public async Task<int> CrearAsync(Rol pRol)
+        {
+            return await RolDAL.CrearAsync(pRol);
+        }
+        public async Task<int> ModificarAsync(Rol pRol)
+        {
+            return await RolDAL.ModificarAsync(pRol);
+        }
+        public async Task<int> EliminarAsync(Rol pRol)
+        {
+            return await RolDAL.EliminarAsync(pRol);
+        }
+        public async Task<Rol> ObtenerPorIdAsync(Rol pRol)
+        {
+            return await RolDAL.OptenerPorIdAsync(pRol);
+        }
+        public async Task<List<Rol>> ObtenerTodosAsync()
+        {
+            return await RolDAL.OptenerTodosAsync();
+        }
+        public async Task<List<Rol>> BuscarAsync(Rol pRol)
+        {
+            return await RolDAL.BuscarAsync(pRol);
+        }
+    }
+}
